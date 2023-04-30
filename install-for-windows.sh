c41661781aa7ed0c77cd7e806b74a75cdcbc6743
mggggk <mggggk@gmail.com>
2023-04-30 12:53:22 +0900

    Fix format

--- install-for-windows.sh
+++ install-for-windows.sh
@@ -4,20 +4,20 @@ set -e
 set -u
 
 # Main script
-now_text=`date +%Y-%m-%d_%H-%M-%S`
+now_text=$(date +%Y-%m-%d_%H-%M-%S)
 old_dir=~/old-shell-conf-$now_text
 
 mkdir -p ~/.config
 
 # bash
-if type bash > /dev/null 2>&1 ; then
+if type bash >/dev/null 2>&1; then
   # Backup if exists
   if [[ -e ~/.config/bash ]]; then
-    mkdir -p $old_dir
-    mv ~/.config/bash $old_dir
-    mv ~/.bash_profile $old_dir
+    mkdir -p "$old_dir"
+    mv ~/.config/bash "$old_dir"
+    mv ~/.bash_profile "$old_dir"
   fi
   cp -r bash ~/.config
   curl -o ~/.config/bash/key-bindings.bash "https://raw.githubusercontent.com/junegunn/fzf/6fb41a202a97ad3f2437f6e5aee8890268560412/shell/key-bindings.bash"
-  echo "source ~/.config/bash/bash_profile" > ~/.bash_profile
+  echo "source ~/.config/bash/bash_profile" >~/.bash_profile
 fi
--- install.sh
+++ install.sh
@@ -4,46 +4,46 @@ set -e
 set -u
 
 # Main script
-now_text=`date +%Y-%m-%d_%H-%M-%S`
+now_text=$(date +%Y-%m-%d_%H-%M-%S)
 old_dir=~/old-shell-conf-$now_text
 
 mkdir -p ~/.config
 
 # zsh
 # 履歴ファイルを作成
-if type zsh > /dev/null 2>&1 ; then
+if type zsh >/dev/null 2>&1; then
   touch ~/.zsh_history
-  chmod 666  ~/.zsh_history
+  chmod 666 ~/.zsh_history
 
   # Backup if exists
   if [[ -e ~/.config/zsh ]]; then
-    mkdir -p $old_dir
-    mv ~/.config/zsh $old_dir
-    mv ~/.zshrc $old_dir
+    mkdir -p "$old_dir"
+    mv ~/.config/zsh "$old_dir"
+    mv ~/.zshrc "$old_dir"
   fi
   cp -r zsh ~/.config
-  echo "source ~/.config/zsh/zshrc" > ~/.zshrc
+  echo "source ~/.config/zsh/zshrc" >~/.zshrc
 fi
 
 # bash
-if type bash > /dev/null 2>&1 ; then
+if type bash >/dev/null 2>&1; then
   # Backup if exists
   if [[ -e ~/.config/bash ]]; then
-    mkdir -p $old_dir
-    mv ~/.config/bash $old_dir
-    mv ~/.bash_profile $old_dir
+    mkdir -p "$old_dir"
+    mv ~/.config/bash "$old_dir"
+    mv ~/.bash_profile "$old_dir"
   fi
   cp -r bash ~/.config
-  echo "source ~/.config/bash/bash_profile" > ~/.bash_profile
+  echo "source ~/.config/bash/bash_profile" >~/.bash_profile
 fi
 
-if type tmux > /dev/null 2>&1 ; then
+if type tmux >/dev/null 2>&1; then
   # Backup if exists
   if [[ -e ~/.config/tmux ]]; then
-    mkdir -p $old_dir
-    mv ~/.config/tmux $old_dir
-    mv ~/.tmux.conf $old_dir
+    mkdir -p "$old_dir"
+    mv ~/.config/tmux "$old_dir"
+    mv ~/.tmux.conf "$old_dir"
   fi
   cp -r tmux ~/.config
-  echo "source-file ~/.config/tmux/tmux.conf" > ~/.tmux.conf
+  echo "source-file ~/.config/tmux/tmux.conf" >~/.tmux.conf
 fi
