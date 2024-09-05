cf5a8e36ae490b7fd691b0e67e8c75a7ab56c937
mggggk <mggggk@gmail.com>
2024-09-05 22:54:30 +0900

    Fix lint warning

--- install.sh
+++ install.sh
@@ -4,46 +4,46 @@ set -e
 set -u
 
 # Main script
-now_text=$(date +%Y-%m-%d_%H-%M-%S)
-old_dir=~/old-shell-conf-$now_text
+now_text="$(date +%Y-%m-%d_%H-%M-%S)"
+old_dir="$HOME/old-shell-conf-$now_text"
 
-mkdir -p ~/.config
+mkdir -p "$HOME/.config"
 
 # zsh
 # 履歴ファイルを作成
-if type zsh >/dev/null 2>&1; then
-  touch ~/.zsh_history
-  chmod 666 ~/.zsh_history
+if type zsh >/dev/null 2>&1 ; then
+  touch "$HOME/.zsh_history"
+  chmod 666  "$HOME/.zsh_history"
 
   # Backup if exists
-  if [[ -e ~/.config/zsh ]]; then
+  if [[ -e "$HOME/.config/zsh" ]]; then
     mkdir -p "$old_dir"
-    mv ~/.config/zsh "$old_dir"
-    mv ~/.zshrc "$old_dir"
+    mv "$HOME/.config/zsh" "$old_dir"
+    mv "$HOME/.zshrc" "$old_dir"
   fi
-  cp -r zsh ~/.config
-  echo "source ~/.config/zsh/zshrc" >~/.zshrc
+  cp -r zsh "$HOME/.config"
+  echo "source ~/.config/zsh/zshrc" >"$HOME/.zshrc"
 fi
 
 # bash
-if type bash >/dev/null 2>&1; then
+if type bash >/dev/null 2>&1 ; then
   # Backup if exists
-  if [[ -e ~/.config/bash ]]; then
+  if [[ -e "$HOME/.config/bash" ]]; then
     mkdir -p "$old_dir"
-    mv ~/.config/bash "$old_dir"
-    mv ~/.bashrc "$old_dir"
+    mv "$HOME/.config/bash" "$old_dir"
+    mv "$HOME/.bash_profile" "$old_dir"
   fi
-  cp -r bash ~/.config
-  echo "source ~/.config/bash/bashrc" >~/.bashrc
+  cp -r bash "$HOME/.config"
+  echo "source ~/.config/bash/bash_profile" >"$HOME/.bash_profile"
 fi
 
-if type tmux >/dev/null 2>&1; then
+if type tmux >/dev/null 2>&1 ; then
   # Backup if exists
-  if [[ -e ~/.config/tmux ]]; then
+  if [[ -e "$HOME/.config/tmux" ]]; then
     mkdir -p "$old_dir"
-    mv ~/.config/tmux "$old_dir"
-    mv ~/.tmux.conf "$old_dir"
+    mv "$HOME/.config/tmux" "$old_dir"
+    mv "$HOME/.tmux.conf" "$old_dir"
   fi
-  cp -r tmux ~/.config
-  echo "source-file ~/.config/tmux/tmux.conf" >~/.tmux.conf
+  cp -r tmux "$HOME/.config"
+  echo "source-file ~/.config/tmux/tmux.conf" >"$HOME/.tmux.conf"
 fi
